308e5ea0b83a444b6f337913b5059b59f1ae0489
Nobebans <isakp@live.se>
2023-02-24 10:46:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Grundämnen
+{
+    internal class Program
+    {
+        public class Grundämnen
+        {
+            public string namn, z, typ, smältpunkt, kokpunkt;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
