6a4023a8e03901249f36c7e969a503703f13c929
Nobebans <isakp@live.se>
2023-02-24 11:09:44 +0100

    skapade foreach loop som printar ut alla metaller

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Grundämnen
             public string namn, z, typ, smältpunkt, kokpunkt;
 
             public void Print()
-            {  
+            {
                 //uppgift 3
                 Console.WriteLine($"Grundämne: {namn}");
                 Console.WriteLine($"Typ: {typ}");
@@ -21,7 +21,7 @@ namespace Grundämnen
         }
 
         static void Main(string[] args)
-        {  
+        {
             //uppgift 2
             Grundämne syre = new Grundämne() { namn = "syre", z = "8", typ = "ickemetall", smältpunkt = "54,36 K", kokpunkt = "90,188 K" };
             Grundämne järn = new Grundämne() { namn = "järn", z = "26", typ = "metall", smältpunkt = "1811 K", kokpunkt = "3134 K" };
@@ -37,7 +37,7 @@ namespace Grundämnen
             grundämnen[0] = syre;
             grundämnen[1] = järn;
             grundämnen[2] = guld;
-            grundämnen[3] = new Grundämne { namn = "väte", z = "1", typ = "ickemetall", smältpunkt = "13,99 K", kokpunkt = "20,271 K"};
+            grundämnen[3] = new Grundämne { namn = "väte", z = "1", typ = "ickemetall", smältpunkt = "13,99 K", kokpunkt = "20,271 K" };
             grundämnen[4] = new Grundämne { namn = "brom", z = "35", typ = "ickemetall", smältpunkt = "265,8 K", kokpunkt = "322 K" };
             grundämnen[5] = new Grundämne { namn = "kvicksilver", z = "80", typ = "metall", smältpunkt = "234,321 K", kokpunkt = "629,88 K" };
 
@@ -45,6 +45,15 @@ namespace Grundämnen
             foreach (Grundämne grundämne in grundämnen)
             {
                 grundämne.Print();
+            }
+
+            //uppgift 6
+            foreach (Grundämne metaller in grundämnen)
+            {
+                if (metaller.typ == "metall")
+                {
+                    metaller.Print();
+                }
             }
         }
     }
