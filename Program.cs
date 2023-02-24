3739a961a324aa2d54c10e4d8c6a88e9cb930281
Nobebans <isakp@live.se>
2023-02-24 11:38:48 +0100

    ändrade så alla attribut inte är strings för att undvika problem med uppgift 7, samt gjorde en foreach loop för att printa ut alla grundämnen med smältpunkt lägre än 273,16 kelvin men med kokpunkt högre än 273,16 kelvin

--- Program.cs
+++ Program.cs
@@ -7,15 +7,20 @@ namespace Grundämnen
         public class Grundämne
         {
             //uppgift 1
-            public string namn, z, typ, smältpunkt, kokpunkt;
+            public string namn;
+            public int z;
+            public string typ;
+            public double smältpunkt;
+            public double kokpunkt;
+
 
             public void Print()
             {
                 //uppgift 3
                 Console.WriteLine($"Grundämne: {namn}");
                 Console.WriteLine($"Typ: {typ}");
-                Console.WriteLine($"Smältpunkt: {smältpunkt}");
-                Console.WriteLine($"Kokpunkt: {kokpunkt}");
+                Console.WriteLine($"Smältpunkt: {smältpunkt} K");
+                Console.WriteLine($"Kokpunkt: {kokpunkt} K");
                 Console.WriteLine();
             }
         }
@@ -23,9 +28,26 @@ namespace Grundämnen
         static void Main(string[] args)
         {
             //uppgift 2
-            Grundämne syre = new Grundämne() { namn = "syre", z = "8", typ = "ickemetall", smältpunkt = "54,36 K", kokpunkt = "90,188 K" };
-            Grundämne järn = new Grundämne() { namn = "järn", z = "26", typ = "metall", smältpunkt = "1811 K", kokpunkt = "3134 K" };
-            Grundämne guld = new Grundämne() { namn = "guld", z = "79", typ = "metall", smältpunkt = "1337,33 K", kokpunkt = "3243 K" };
+            Grundämne syre = new Grundämne();
+            syre.namn = "syre";
+            syre.z = 8;
+            syre.typ = "ickemetall";
+            syre.smältpunkt = 54.36;
+            syre.kokpunkt = 90.188;
+
+            Grundämne järn = new Grundämne();
+            järn.namn = "järn";
+            järn.z = 26;
+            järn.typ = "metall";
+            järn.smältpunkt = 1811;
+            järn.kokpunkt = 3134;
+
+            Grundämne guld = new Grundämne();
+            guld.namn = "guld";
+            guld.z = 79;
+            guld.typ = "metall";
+            guld.smältpunkt = 1337.33;
+            guld.kokpunkt = 3243;
 
             //uppgift 3
             syre.Print();
@@ -37,9 +59,28 @@ namespace Grundämnen
             grundämnen[0] = syre;
             grundämnen[1] = järn;
             grundämnen[2] = guld;
-            grundämnen[3] = new Grundämne { namn = "väte", z = "1", typ = "ickemetall", smältpunkt = "13,99 K", kokpunkt = "20,271 K" };
-            grundämnen[4] = new Grundämne { namn = "brom", z = "35", typ = "ickemetall", smältpunkt = "265,8 K", kokpunkt = "322 K" };
-            grundämnen[5] = new Grundämne { namn = "kvicksilver", z = "80", typ = "metall", smältpunkt = "234,321 K", kokpunkt = "629,88 K" };
+
+            grundämnen[3] = new Grundämne();
+            grundämnen[3].namn = "väte";
+            grundämnen[3].z = 1;
+            grundämnen[3].typ = "ickemetall";
+            grundämnen[3].smältpunkt = 13.99;
+            grundämnen[3].kokpunkt = 20.271;
+
+
+            grundämnen[4] = new Grundämne();
+            grundämnen[4].namn = "brom";
+            grundämnen[4].z = 35;
+            grundämnen[4].typ = "ickemetall";
+            grundämnen[4].smältpunkt = 265.8;
+            grundämnen[4].kokpunkt = 322;
+
+            grundämnen[5] = new Grundämne();
+            grundämnen[5].namn = "kvicksilver";
+            grundämnen[5].z = 80;
+            grundämnen[5].typ = "metall";
+            grundämnen[5].smältpunkt = 234.321;
+            grundämnen[5].kokpunkt = 629.88;
 
             //uppgift 5
             foreach (Grundämne grundämne in grundämnen)
@@ -53,6 +94,15 @@ namespace Grundämnen
                 if (metaller.typ == "metall")
                 {
                     metaller.Print();
+                }
+            }
+
+            //uppgift 7
+            foreach (Grundämne grundämne in grundämnen)
+            {
+                if (grundämne.smältpunkt < 273.16 && grundämne.kokpunkt > 273.16)
+                {
+                    grundämne.Print();
                 }
             }
         }
