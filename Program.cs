a83f45f01760b32d7b441e62a20f037b642b4c94
Nobebans <isakp@live.se>
2023-02-24 10:55:31 +0100

    Print metod samt printa ut syre, järn, guld

--- Program.cs
+++ Program.cs
@@ -5,13 +5,26 @@
         public class Grundämne
         {
             public string namn, z, typ, smältpunkt, kokpunkt;
+
+            public void Print()
+            {
+                Console.WriteLine($"Grundämne: {namn}");
+                Console.WriteLine($"Typ: {typ}");
+                Console.WriteLine($"Smältpunkt: {smältpunkt}");
+                Console.WriteLine($"Kokpunkt: {kokpunkt}");
+                Console.WriteLine();
+            }
         }
 
         static void Main(string[] args)
         {
-            Grundämne syre = new Grundämne() { namn = "syre", z = "8", typ = "ickemetall", smältpunkt = "54,36", kokpunkt = "90,188"};
-            Grundämne järn = new Grundämne() { namn = "järn", z = "26", typ = "metall", smältpunkt = "1811", kokpunkt = "3134" };
-            Grundämne guld = new Grundämne() { namn = "guld", z = "79", typ = "metall", smältpunkt = "1337,33", kokpunkt = "3243" };
+            Grundämne syre = new Grundämne() { namn = "syre", z = "8", typ = "ickemetall", smältpunkt = "54,36 K", kokpunkt = "90,188 K" };
+            Grundämne järn = new Grundämne() { namn = "järn", z = "26", typ = "metall", smältpunkt = "1811 K", kokpunkt = "3134 K" };
+            Grundämne guld = new Grundämne() { namn = "guld", z = "79", typ = "metall", smältpunkt = "1337,33 K", kokpunkt = "3243 K" };
+
+            syre.Print();
+            järn.Print();
+            guld.Print();
         }
     }
 }
